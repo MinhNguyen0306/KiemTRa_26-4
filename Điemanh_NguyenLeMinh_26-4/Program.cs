ad6336d7cfed9dce5ff34176090fb51f985064bb
Nguyen Le Minh <minhnguyen183385@gmail.com>
2022-04-26 08:07:26 +0700

    diemdanh

---
+++ Điemanh_NguyenLeMinh_26-4/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Điemanh_NguyenLeMinh_26_4
+{
+    class PTB2_235
+    {
+        private double a_235, b_235, c_235;
+
+        public PTB2_235(){}
+
+        public PTB2_235(double a_235, double b_235, double c_235){ }
+
+        public double Geta_235()
+        {
+            return a_235;
+        }
+
+        public void Seta_235(double a235)
+        {
+            a_235 = a235;
+        }
+
+        public double Getb_235()
+        {
+            return b_235;
+        }
+
+        public void Setb_235(double b235)
+        {
+            b_235 = b235;
+        }
+
+        public double Getc_235()
+        {
+            return c_235;
+        }
+
+        public void Setc_235(double c235)
+        {
+            c_235 = c235;
+        }
+
+        public static double delta(double a235, double b235, double c235)
+        {
+            double d = Math.Pow(b235, 2) - (4 * a235 * c235);
+            return d;
+        }
+
+        public static void nghiem(double a235, double b235, double c235)
+        {
+            double x1_235, x2_235;
+            x1_235 = (b235 - Math.Sqrt(delta(a235,b235,c235))) / (2 * a235);
+            x2_235 = (b235 + Math.Sqrt(delta(a235, b235, c235))) / (2 * a235);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PTB2_235 b2_235 = new PTB2_235();
+            b2_235.Seta_235(2);
+            b2_235.Setb_235(3);
+            b2_235.Setc_235(4);
+            Console.WriteLine("Nghiem pt bac 2 la: " + PTB2_235.delta(b2_235.Geta_235(), b2_235.Getb_235(), b2_235.Getc_235());
+        }
+    }
+}
---
+++ Điemanh_NguyenLeMinh_26-4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Điemanh_NguyenLeMinh_26-4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Điemanh_NguyenLeMinh_26-4")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4427c505-744f-49b3-8a8f-70f1291ef3ec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
